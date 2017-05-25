21b01e92f3020c0bd73f8e524e019f657f93ece1
Milton <milton@sinnovation.com>
2017-05-25 13:18:21 +0530

    fixed import error on repo

--- test/test_fibo.py
+++ test/test_fibo.py
@@ -1,7 +1,8 @@
-import sys
+import sys, os; sys.path.insert(0, os.path.abspath('..'));
+''' Some reason below import is not working'''
+"""import sys
 # feb dir contains fibo.py
-sys.path.append('example/feb')
-
+sys.path.append('example/feb')"""
 
 from feb.fibo import fib
 print (fib(500))
